8567039c0a6292e27aba5fb03491b4253a8c5cca
Noszály Áron <noszalyaron4@gmail.com>
2020-08-18 09:43:30 +0200

    basic loop

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Person struct {
+	Name string
+	Phone string
+}
+
+var people = make([]Person, 0)
+
+var commands = map[string]func([]string){
+	"quit": func(args []string) {
+		os.Exit(0)
+	},
+	"help": func(args []string) {
+		fmt.Println("@TODO")
+	},
+}
 
 func main() {
-	fmt.Println("phonebook")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("PB> ")
+		line, _ := reader.ReadString('\n')
+
+		args := strings.Fields(line)
+		command := args[0]
+		if _, ok := commands[command]; !ok {
+			fmt.Printf("unknown command: %s\n", command)
+		}else {
+			commands[command](args)
+		}
+	}
 }
